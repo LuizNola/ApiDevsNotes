ffa7d29427129bb4a1681e0acff944a213c14d14
Luiz Nola <luizn647@gmail.com>
2021-01-29 22:25:21 -0300

    Configurando os EndPoints

--- src/controllers/NoteController.js
+++ src/controllers/NoteController.js
@@ -1,5 +1,11 @@
 const NoteService = require('../Models/NoteService')
 
 module.exports = {
-    ping: (req, res) => {res.json({pong: true})}
+    ping: (req, res) => {res.json({pong: true})},
+    
+    all: ()=>{},
+    one: ()=>{},
+    new: ()=>{},
+    edit: ()=>{},
+    delete: ()=>{}
 };
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,10 @@ const router = express.Router();
 const NoteController = require('./controllers/NoteController')
 
 router.get('/ping', NoteController.ping )
+router.get('/notes', NoteController.all )
+router.get('/note/:id', NoteController.one )
+router.post('/note', NoteController.new )
+router.put('/note/:id', NoteController.edit)
+router.delete('/note/:id', NoteController.delete)
 
 module.exports = router;
