4048db4778e47916a96c4d917f292bd55f86388d
Luiz Nola <luizn647@gmail.com>
2021-01-30 17:48:46 -0300

    Feita a Função de listagem

--- src/Models/NoteService.js
+++ src/Models/NoteService.js
@@ -1,5 +1,13 @@
-const db = require('../db')
+const db = require('../db');
 
 module.exports = {
-    
+  getAll: ()=>{
+      return new Promise((resolve, reject)=>{
+          
+        db.query('SELECT * FROM notes_tb', (error, results)=>{
+            if(error){reject(error);   return;}
+            resolve(results);
+        });
+      });
+  }
 };
--- src/controllers/NoteController.js
+++ src/controllers/NoteController.js
@@ -3,8 +3,19 @@ const NoteService = require('../Models/NoteService')
 module.exports = {
     ping: (req, res) => {res.json({pong: true})},
     
-    all: ()=>{
+    all: async (req, res)=>{
+        let json = {error:'',result:[]};
 
+        let notes = await NoteService.getAll();
+
+        for(let i in notes){
+            json.result.push({
+                id:notes[i].id,
+                title:notes[i].title
+            })
+        }
+
+        res.json(json);
     },
     one: ()=>{
 
